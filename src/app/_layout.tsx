16240d7a55a6365635e68b53d77bddcdea655ab9
Roberto Mazor <mazor.estudo@gmail.com>
2026-08-06 21:32:53 -0300

    .

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,8 @@
 import { Stack } from "expo-router";
 
+import "../src/global.css";
+import { Slot } from "expo-router";
+
 export default function RootLayout() {
   return <Stack screenOptions={{headerShown: false}}/>;
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import "@/global.css"; 
 
-export default function Index() {
+export default function Page() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Ola Mundo.</Text>
+    <View className="flex-1 justify-center items-center bg-background">
+      <Text className="text-xl font-bold text-indigo-PRIMARY">
+        Indigo Finance Mobile
+      </Text>
     </View>
   );
 }
