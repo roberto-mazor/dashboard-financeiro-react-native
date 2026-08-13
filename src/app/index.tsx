72af6bc9b69de47016357d99fed175ed217089ef
Roberto Mazor <mazor.estudo@gmail.com>
2026-08-12 21:38:08 -0300

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,38 @@
-import { Text, View } from "react-native";
-import "@/global.css"; 
+import React, { useEffect } from 'react';
+import { View, ActivityIndicator } from 'react-native';
+import { router } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
+import { useAuth } from '../contexts/AuthContext';
 
-export default function Page() {
+export default function Index() {
+  const { usuario, carregando } = useAuth();
+
+  useEffect(() => {
+    async function verificarFluxo() {
+      if (carregando) return;
+
+      // 1. Se o usuário já está logado, vai para o Dashboard 
+      if (usuario) {
+        //IMPORTANTE Por enquanto direcionado para login até ter o dashboard
+        return;
+      }
+
+      // 2. Verificar se o onboarding já foi visto
+      const onboardingVisto = await SecureStore.getItemAsync('@IndigoFinance:onboarding_visto');
+
+      if (onboardingVisto === 'true') {
+        router.replace('/login');
+      } else {
+        router.replace('/onboarding');
+      }
+    }
+
+    verificarFluxo();
+  }, [usuario, carregando]);
+
   return (
-    <View className="flex-1 justify-center items-center bg-background">
-      <Text className="text-xl font-bold text-indigo-PRIMARY">
-        Indigo Finance Mobile
-      </Text>
+    <View className="flex-1 bg-background justify-center items-center">
+      <ActivityIndicator size="large" color="#4f46e5" />
     </View>
   );
 }
